0da3f96a410c8e96d81dfc679cee914e5a63e720
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-06-22 16:33:09 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -2,9 +2,9 @@ $folder = "src\T01_intro"
 npm run html $folder
 npm run pdf $folder
 
-$folder = "src\T02_js_ts"
-npm run html $folder
-npm run pdf $folder
+# $folder = "src\T02_js_ts"
+# npm run html $folder
+# npm run pdf $folder
 
 # $folder = "src\T03_pf_db"
 # npm run html $folder
