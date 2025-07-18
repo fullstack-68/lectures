02c649b16022fc0f04a3b2244259e19a2bdd7cd7
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-07-18 15:50:11 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -26,9 +26,9 @@ $folder = "src\T06_pf_testing"
 npm run html $folder
 npm run pdf $folder
 
-# $folder = "src\T07_pf_deploy"
-# npm run html $folder
-# npm run pdf $folder
+$folder = "src\T07_pf_deploy"
+npm run html $folder
+npm run pdf $folder
 
 # $folder = "src\T08_landscape"
 # npm run html $folder
